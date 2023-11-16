a6e29ca2fbbdc366d0156d83d317c13e0c20487f
sudesh2022 <aisudesh777@gmail.com>
2023-11-17 07:05:10 +0800

    publish

---
+++ portfolio.py
@@ -0,0 +1,91 @@
+# import the Streamlit library
+import streamlit as st
+from streamlit_option_menu import option_menu
+from utils.constants import *
+
+# configure page settings
+st.set_page_config(page_title='Template' ,layout="wide",initial_sidebar_state="auto", page_icon='👧🏻') # always show the sidebar
+
+# load local CSS styles
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("styles/styles_main.css")
+
+# get the variables from constants.py
+pronoun = info['Pronoun']
+
+# app sidebar
+with st.sidebar:
+    st.markdown("""
+                # Chat with my AI assistant
+                """)
+    with st.expander("Click here to see FAQs"):
+        st.info(
+            f"""
+            - What are {pronoun} strengths and weaknesses?
+            - What is {pronoun} expected salary?
+            - What is {pronoun} latest project?
+            - When can {pronoun} start to work?
+            - Tell me about {pronoun} professional background
+            - What is {pronoun} skillset?
+            - What is {pronoun} contact?
+            - What are {pronoun} achievements?
+            """
+        )
+        
+    st.caption(f"Vicky Kuo is the original developer")
+
+import requests
+
+def hero(content1, content2):
+    st.markdown(f'<h1 style="text-align:center;font-size:60px;border-radius:2%;">'
+                f'<span>{content1}</span><br>'
+                f'<span style="color:black;font-size:22px;">{content2}</span></h1>', 
+                unsafe_allow_html=True)
+
+with st.container():
+    col1,col2 = st.columns([8,3])
+
+full_name = info['Full_Name']
+with col1:
+    hero(f"Hi, I'm {full_name}👋", info["Intro"])
+    st.write("")
+    st.write(info['About'])
+
+    from streamlit_extras.switch_page_button import switch_page
+    col_1, col_2, temp = st.columns([0.35,0.2,0.45])
+    with col_1:
+        btn1 = st.button("Chat with My AI Assistant")
+        if btn1:
+            switch_page("AI_Assistant_Chat")
+    with col_2:
+        btn2 = st.button("My Resume")
+        if btn2:
+            switch_page("Resume")
+
+import streamlit.components.v1 as components
+
+def change_button_color(widget_label, background_color='transparent'):
+    htmlstr = f"""
+        <script>
+            var elements = window.parent.document.querySelectorAll('button');
+            for (var i = 0; i < elements.length; ++i) {{ 
+                if (elements[i].innerText == '{widget_label}') {{ 
+                    elements[i].style.background = '{background_color}'
+                }}
+            }}
+        </script>
+        """
+    components.html(f"{htmlstr}", height=0, width=0)
+
+change_button_color('Chat with My AI Assistant', '#0cc789') 
+
+from  PIL import Image
+with col2:
+    profile = Image.open("images/profile.png")
+    st.image(profile, width=280)
+          
+with st.container():
+    st.write("---")
---
+++ utils/constants.py
@@ -0,0 +1,122 @@
+chat = {
+    "TITLE1": "About Me",
+    "TITLE2": "AI Chat",
+    "menu_v": {
+   "container": {"background-color": "#a7c5ff"},
+   "icon": {"color": "white", "font-size": "25px"}, 
+   "nav-link": {"font-size": "25px", "text-align": "center", "color": "white", "margin":"0px", "--hover-color": "#1b97ff"},
+   "nav-link-selected": {"background-color": "#1b97ff"}
+    },
+    "menu_h": {
+        "container": {"padding": "0px",
+                      "display": "grid",
+                      "margin": "0!important",
+                      "background-color": "#212121"
+                      },
+        "icon": {"color": "#bd93f9", "font-size": "14px"},
+        "nav-link": {
+            "font-size": "14px",
+            "text-align": "center",
+            "margin": "auto",
+            "background-color": "#262626",
+            "height": "30px",
+            "width": "13rem",
+            "color": "#edff85",
+            "border-radius": "5px"
+        },
+        "nav-link-selected": {
+            "background-color": "#212121",
+            "font-weight": "300",
+            "color": "#f7f8f2",
+            "border": "1px solid #e55381"
+        }
+    }
+}
+
+info = {
+   "Pronoun": "his", 
+   "Subject": "he", 
+   "Name": "Sudesh",
+   "Full_Name":"Sudesh", 
+   "Intro": "A Cloud Architect and AI Enthusiast at IBM Consulting",
+   "About":"Hello, I'm Sudesh, and I'm passionate about leveraging data and technology to drive meaningful insights and solutions in business.",
+   "Project":"https://www.linkedin.com/in/ksudesh/",
+   "Medium":"https://medium.com/@sudeshkrishnamoorthy/about",
+   "City":"Singapore, Singapore",
+   "Resume": "https://drive.google.com/file/d/132P3uf3UEtvljWGCOc5dTjDj6lP--99N/view?usp=sharing",
+   "Email": "sudeshk@msn.com" 
+}
+
+projects = [
+        {
+            "title": "Unlocking Multilingual Magic: Babel Fish with LLM STT TTS",
+            "description": "Text-to-Speech, Speech-To-Text, LLM, watsonx",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/babel.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX0PPIEN+v1"
+        },
+        {
+            "title": "Summarize Your Private Data with LLMs & Generative AI",
+            "description": "LLM, watsonx, watsonx.ai",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/summary.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX0DTPEN+v1"
+        },      
+        {
+            "title": "Improve Customer Support Efficiency with Open-Source LLM",
+            "description": "Generative AI",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/CS.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX0FUSEN+v1"
+        },
+        {
+            "title": "A Quick Introduction to Machine Learning",
+            "description": "Machine Learning",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/ML.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+ML0104EN+v1"
+        },
+        {
+            "title": "Reinforcement Learning and Deep Learning Essentials",
+            "description": "Reinforcement Learning, Keras, PyTorch",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/RL.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+ML0105EN+v1"
+        },
+        {
+            "title": "Automate ML Pipelines Using Apache Airflow",
+            "description": "Machine Learning Pipeline",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/airflow.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBM+GPXX0DNQEN+v1"
+        },
+        {
+            "title": "Create Your Own ChatGPT-like Website with Open Source LLMs",
+            "description": "LLM, Chatbot, Generative AI",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/chatgpt.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX04ESEN+v1"
+        },
+        {
+            "title": "A Quick Introduction to Machine Learning",
+            "description": "Machine Learning",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/ML.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+ML0104EN+v1"
+        },
+        {
+            "title": "Build an AI Web App for Diamond Price Prediction",
+            "description": "Machine Learning, Data Science",
+            "image_url": "https://sn-assets.s3.us.cloud-object-storage.appdomain.cloud/projects/diamond.png",
+            "link": "https://cognitiveclass.ai/courses/course-v1:IBMSkillsNetwork+GPXX0FTNEN+v1"
+        }                  
+    ]
+
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
